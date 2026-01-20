56e87d20c454255f6b39e1838f1206b0fecd0084
Aaron Raymundo Juarez <je25001@esfe.agape.edu.sv>
2026-01-20 14:17:36 -0600

    Layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,17 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login';
 import { HomeComponent } from './home/home';
 import { PermisoForm } from './permiso-form/permiso-form';
+import { LayoutComponent } from './layout/layout';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
-  { path: 'home', component: HomeComponent },
-  { path: 'permiso', component: PermisoForm }
+
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: 'home', component: HomeComponent },
+      { path: 'permiso', component: PermisoForm }
+    ]
+  }
 ];
